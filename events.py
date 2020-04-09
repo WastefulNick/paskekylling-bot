8b2bcf48232b08aae487c75322954dece03327f8
Andreas Tvete <andreas.roed.tvete@gmail.com>
2020-04-09 12:41:19 +0200

    check for edits, untested :)

--- events.py
+++ events.py
@@ -16,7 +16,18 @@ class EventsCog(commands.Cog, name="Events"):
                 if msg.find('cryptobin.co') == -1:
                     await message.delete()
                     await message.channel.send('Meldingen din ble slettet fra <#656583866600914953> fordi den ikke inneholdt en cryptobin link. Du kan diskutere løsningene i <#652630061584875532>', delete_after=5)
-            
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        msg = after.content.lower()
+
+        cb_chnl_ids = [656583866600914953, 697736009357721630]
+
+        for chnl_id in cb_chnl_ids:
+            if after.channel.id == chnl_id:
+                if msg.find('cryptobin.co') == -1:
+                    await after.delete()
+                    await after.channel.send('Meldingen din ble slettet fra <#656583866600914953> fordi den ikke inneholdt en cryptobin link. Du kan diskutere løsningene i <#652630061584875532>', delete_after=5)            
 
 def setup(bot):
     bot.add_cog(EventsCog(bot))
