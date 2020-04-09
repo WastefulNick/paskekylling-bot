815e93c990e6f932212efd9df77b45d5e15772dc
Andreas Tvete <andreas.roed.tvete@gmail.com>
2020-04-09 12:39:51 +0200

    New cryptobin channel

--- events.py
+++ events.py
@@ -9,12 +9,13 @@ class EventsCog(commands.Cog, name="Events"):
     async def on_message(self, message):
         msg = message.content.lower()
 
-        cb_chnl_id = 656583866600914953
+        cb_chnl_ids = [656583866600914953, 697736009357721630]
 
-        if message.channel.id == cb_chnl_id:
-            if msg.find('cryptobin.co') == -1:
-                await message.delete()
-                await message.channel.send('Meldingen din ble slettet fra <#656583866600914953> fordi den ikke inneholdt en cryptobin link. Du kan diskutere løsningene i <#652630061584875532>', delete_after=5)
+        for chnl_id in cb_chnl_ids:
+            if message.channel.id == chnl_id:
+                if msg.find('cryptobin.co') == -1:
+                    await message.delete()
+                    await message.channel.send('Meldingen din ble slettet fra <#656583866600914953> fordi den ikke inneholdt en cryptobin link. Du kan diskutere løsningene i <#652630061584875532>', delete_after=5)
             
 
 def setup(bot):
