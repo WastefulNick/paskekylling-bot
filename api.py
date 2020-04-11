032a78c3acf5ff885acf7e05befe0a890e8a0bab
Andreas Tvete <andreas.roed.tvete@gmail.com>
2020-04-11 22:20:18 +0200

    Added timeouts

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ s = requests.Session()
 
 class API():
     def startSession(self, npst_pass):
-        req = s.get('https://ctf.phst.no/login')
+        req = s.get('https://ctf.phst.no/login', timeout=5)
         soup = BeautifulSoup(req.text, 'html.parser')
         nonce = soup.findAll('input', {'name': 'nonce'})[0]['value']
         data = {
@@ -14,10 +14,10 @@ class API():
             'name': 'Påskekylling BOT',
             'nonce': nonce
         }
-        s.post('https://ctf.phst.no/login', data=data)
+        s.post('https://ctf.phst.no/login', data=data, timeout=5)
 
     def getID(self, challname): 
-        r = s.get(url='https://ctf.phst.no/api/v1/challenges')
+        r = s.get(url='https://ctf.phst.no/api/v1/challenges', timeout=5)
         parsed = json.loads(r.text[23:-2])
         for item in parsed:
             if item['name'].lower() == challname.lower():
@@ -25,7 +25,7 @@ class API():
         return 0
 
     def getChalls(self): 
-        r = s.get(url='https://ctf.phst.no/api/v1/challenges')
+        r = s.get(url='https://ctf.phst.no/api/v1/challenges', timeout=5)
         parsed = json.loads(r.text[23:-2])
         challs = []
         for item in parsed:
@@ -33,7 +33,7 @@ class API():
         return challs
 
     def getChallScore(self, id):
-        r = s.get(url=f'https://ctf.phst.no/api/v1/challenges/{id}/solves')
+        r = s.get(url=f'https://ctf.phst.no/api/v1/challenges/{id}/solves', timeout=5)
         parsed = json.loads(r.text[23:-2])
         names = []
         for x, item in enumerate(parsed):
@@ -44,5 +44,5 @@ class API():
         return names
 
     def getScoreBoard(self):
-        r = s.get(url='https://ctf.phst.no/scoreboard')
+        r = s.get(url='https://ctf.phst.no/scoreboard', timeout=5)
         return BeautifulSoup(r.text, 'html.parser')
